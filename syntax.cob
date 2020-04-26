baad7d783883913466d5d33c3108f9a0b0a48b3b
noranuko13 <noranuko13@gmail.com>
2020-04-26 19:22:35 +0900

    :notes: SYNTAX 標識領域は7列目

--- syntax.cob
+++ syntax.cob
@@ -1,34 +1,34 @@
-       *** 見出し部
+      *** 見出し部
        IDENTIFICATION  DIVISION.
 
-       * プログラム名
+      * プログラム名
        PROGRAM-ID.     SYNTAX.
 
 
-       *** 環境部
+      *** 環境部
        ENVIRONMENT     DIVISION.
 
 
-       *** データ部
+      *** データ部
        DATA            DIVISION.
-       ** 作業場所節
+      ** 作業場所節
        WORKING-STORAGE SECTION.
-       * 利用者定義語
-       *   平たくいえば定数・変数定義
-       *       01      項目レベル
-       *               HTMLの見出しみたいに大きい数字ほど細かい項目
-       *               奇数で書いておいて付け足したい時に間を使う
-       *       CNT     任意の名称
-       *       PIC     PICTURE句
-       *               必須の決まり文句
-       *       9(3)    ＝3桁の数字
-       *               9   数字
-       *               X   英字
-       *               N   日本語
-       *       VALUE 0 初期値設定
+      * 利用者定義語
+      *   平たくいえば定数・変数定義
+      *       01      項目レベル
+      *               HTMLの見出しみたいに大きい数字ほど細かい項目
+      *               奇数で書いておいて付け足したい時に間を使う
+      *       CNT     任意の名称
+      *       PIC     PICTURE句
+      *               必須の決まり文句
+      *       9(3)    ＝3桁の数字
+      *               9   数字
+      *               X   英字
+      *               N   日本語
+      *       VALUE 0 初期値設定
        01 CNT PIC 9(3) VALUE 0.
 
-       * 独立・集団・基本項目
+      * 独立・集団・基本項目
        01 DOG.
            03 REGI-NUM     PIC 9(6)    VALUE 123456.
            03 NAME         PIC  N(30)  VALUE "ぽち".
@@ -38,18 +38,18 @@
                05 DT        PIC 99    VALUE 07.
 
 
-       *** 手続き部
+      *** 手続き部
        PROCEDURE       DIVISION.
-       *   * 画面に文字列を表示する
+      *    * 画面に文字列を表示する
            DISPLAY "Coding Style & Syntax".
 
-       *   * ループ・繰り返し処理
+      *    * ループ・繰り返し処理
            PERFORM 10 TIMES
                ADD 1 TO CNT
                DISPLAY "COUNT = " CNT
            END-PERFORM
 
-       *   * 集団項目の表示
+      *    * 集団項目の表示
            DISPLAY DOG
            DISPLAY DT IN DOG
 
